668dd90c869b97817bf0a69a7d7405e77331de70
Alex Salisbury <alex@alexsalisbury.com>
2020-05-31 16:42:37 -0400

    Add a stub of webservice.

---
+++ Home.DotNet/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿namespace Home.DotNet.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+    }
+}
