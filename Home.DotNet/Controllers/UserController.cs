704ca91bf83439f1378f6e632961814d3081d623
Alex Salisbury <alex@alexsalisbury.com>
2020-05-31 16:56:22 -0400

    Stub in User Post endpoint.

--- Home.DotNet/Controllers/UserController.cs
+++ Home.DotNet/Controllers/UserController.cs
@@ -4,12 +4,25 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
 
+    public class User
+    {
+
+    }
+
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class UserController : ControllerBase
     {
+        [HttpPost]
+        public async Task<IActionResult> PostUserAsync([FromBody] User user)
+        {
+            //Save to Store
+            return Ok();
+        }
     }
 }
