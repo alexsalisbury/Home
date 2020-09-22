dc37e2350fee85082c49f66250140885af915c0c
Alex Salisbury <alex@alexsalisbury.com>
2020-09-22 16:58:09 -0400

    Add LogViewModel and LineMessage to Models

---
+++ Home.App/Models/LineMessage.cs
@@ -0,0 +1,29 @@
+﻿// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Home.App.Models
+{
+    public enum Icon
+    {
+        Unknown,
+        Home,
+        Windows = 10,
+        Azure = 100,
+        Discord = 1000,
+
+    }
+
+    public enum Level
+    {
+        Unknown = 0,
+        Information = 1,
+        Warning = 2,
+        Error = 3
+    } 
+
+    public class LineMessage
+    {
+        public string Text { get; set; }
+        public Icon Icon { get; set; }
+        public Level Level { get; set; }
+    }
+}
---
+++ Home.App/Models/LogViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Windows.UI.Xaml.Media;
+using Windows.UI;
+using System.Collections.ObjectModel;
+using Home.App.Models;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Home.App.Models
+{
+    public class LogViewModel
+    {
+        public int HeaderIconHeight = 8;
+        public int CommandIconHeight = 8;
+        public int LineIconHeight = 8;
+
+        public int CommandText = 8;
+        public int LineText = 8;
+
+        public SolidColorBrush Error = new SolidColorBrush(Colors.Red);
+        public SolidColorBrush Warning = new SolidColorBrush(Colors.Yellow);
+        public SolidColorBrush Online = new SolidColorBrush(Colors.YellowGreen);
+        public SolidColorBrush Discord = new SolidColorBrush(Colors.MediumSlateBlue);
+        public SolidColorBrush Home = new SolidColorBrush(Colors.Orange);
+        public SolidColorBrush Azure = new SolidColorBrush(Colors.CornflowerBlue);
+        public SolidColorBrush Windows = new SolidColorBrush(Colors.CadetBlue);
+        public SolidColorBrush Weather = new SolidColorBrush(Colors.WhiteSmoke);
+
+        public ObservableCollection<LineMessage> Message { get; set; } = new ObservableCollection<LineMessage>();
+    }
+}
