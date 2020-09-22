f0c4dd1b29158b03edaa86ac0ed9dee56834f96a
Alex Salisbury <alex@alexsalisbury.com>
2020-09-22 18:04:13 -0400

    Add LiveActivity, weather Icons and update LineMessage with IsWeather and Status

--- Home.App/Models/LineMessage.cs
+++ Home.App/Models/LineMessage.cs
@@ -5,11 +5,12 @@ namespace Home.App.Models
     public enum Icon
     {
         Unknown,
-        Home,
+        Home = 1,
         Windows = 10,
         Azure = 100,
-        Discord = 1000,
-
+        Discord = 200,
+        WeatherCloudy = 920,
+        WeatherSnowy = 970,
     }
 
     public enum Level
@@ -20,10 +21,23 @@ namespace Home.App.Models
         Error = 3
     } 
 
+    public enum LineActivity
+    {
+        Unknown = 0,
+        NeverActive = 1,
+        Running = 50,
+        Complete = 80,
+        SelfRemovalQueued = 97,
+        RemovalQueued = 98,
+        Removed = 99,
+    }
+
     public class LineMessage
     {
+        public bool IsWeather => Icon == Icon.WeatherCloudy || Icon == Icon.WeatherSnowy;
         public string Text { get; set; }
         public Icon Icon { get; set; }
         public Level Level { get; set; }
+        public LineActivity Status { get; set; }
     }
 }
