af6e697b058e513fa6735bed7d95a51ef4d470b4
Alex Salisbury <alex@alexsalisbury.com>
2020-09-22 17:06:56 -0400

    Add EARLY versions of Command and CommandTemplates to models.

---
+++ Home.App/Models/Command.cs
@@ -0,0 +1,57 @@
+﻿using MahApps.Metro.IconPacks;
+using Windows.System;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Home.App.Models
+{
+
+    public enum IconType
+    {
+        Unknown = 0,
+        FontAwesome = 250,
+        Material = 520,
+        ModernKind = 550
+    }
+
+    public enum CommandType
+    {
+        Unknown = 0,
+        Discord = 150,
+        Windows = 900
+    }
+
+    public class Command
+    {
+        public CommandTemplate Template = CommandTemplate.DefaultUnknownTemplate;
+
+        public bool IsPrefixValid => Template?.IsValid ?? false;
+        public bool IsValid => Template?.IsValid ?? false;
+        public CommandType CommandType => Template?.CommandType ?? CommandType.Unknown;
+        public PackIconModernKind ModernIcon => Template?.ModernIcon ?? PackIconModernKind.Home;
+        public PackIconFontAwesomeKind FontAwesomeIcon => Template?.FontAwesomeIcon ?? PackIconFontAwesomeKind.None;
+        public IconType IconType => Template?.IconType ?? IconType.Unknown;
+
+        public Command(string text, VirtualKey key)
+        {
+            var candidate = CommandTemplate.GetCommandTemplate(text, key);
+            Template = candidate;
+        }
+
+        internal static Command GenerateCandidate(string text, VirtualKey key)
+        {
+            try
+            {
+                var command = new Command(text, key);
+
+                return command;
+            }
+            // catch and log
+            catch
+            {
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Home.App/Models/CommandTemplate.cs
@@ -0,0 +1,89 @@
+﻿using MahApps.Metro.IconPacks;
+using Windows.System;
+using System.Collections.Concurrent;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Home.App.Models
+{
+    public class CommandTemplate
+    {
+        static ConcurrentDictionary<char, CommandTemplate> DefaultTemplates = new ConcurrentDictionary<char, CommandTemplate>();
+        static ConcurrentDictionary<string, CommandTemplate> CommandTemplates = new ConcurrentDictionary<string, CommandTemplate>();
+        public static CommandTemplate DefaultUnknownTemplate = new CommandTemplate();
+        public static CommandTemplate DefaultDiscordTemplate = new CommandTemplate() { IconType = IconType.FontAwesome, IsPrefixValid = true, FontAwesomeIcon = PackIconFontAwesomeKind.DiscordBrands };
+        public static CommandTemplate DefaultWindowsTemplate = new CommandTemplate() { IconType = IconType.Material, IsPrefixValid = true, MaterialIcon = PackIconMaterialKind.MicrosoftWindows};
+
+        public CommandType CommandType { get; private set; } = CommandType.Unknown;
+        public bool IsPrefixValid { get; private set; } = false;
+        public bool IsValid { get; private set; } = false;
+        public PackIconModernKind ModernIcon { get; private set; } = PackIconModernKind.Home;
+        public PackIconFontAwesomeKind FontAwesomeIcon { get; private set; } = PackIconFontAwesomeKind.None;
+        public PackIconMaterialKind MaterialIcon { get; private set; } = PackIconMaterialKind.None;
+        public IconType IconType { get; private set; } = IconType.ModernKind;
+
+        static CommandTemplate()
+        {
+            DefaultTemplates.TryAdd('d', DefaultDiscordTemplate);
+            DefaultTemplates.TryAdd('w', DefaultWindowsTemplate);
+        }
+
+        public CommandTemplate()
+        {
+
+        }
+
+        public static CommandTemplate GetCommandDefaultTemplate(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return DefaultUnknownTemplate;
+            }
+
+            var spaceindex = text.IndexOf(' ');
+            if (spaceindex == 1)
+            {
+                var pref = text[0];
+                var isPrefixValid = CommandTemplate.IsPrefixKnown(pref);
+
+                if (isPrefixValid)
+                {
+                    return DefaultTemplates[pref];
+                }
+            }
+
+            return DefaultUnknownTemplate;
+        }
+
+        public static CommandTemplate GetCommandTemplate(string text, VirtualKey key)
+        {
+            if (key == VirtualKey.Space)
+            {
+                text = text + " ";
+            }
+            
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return DefaultUnknownTemplate;
+            }
+
+            var spaceindex = text.IndexOf(' ');
+            if (spaceindex == 1)
+            {
+                var end = text.IndexOf(' ', spaceindex);
+                if (end > spaceindex)
+                {
+                    var command = text.Substring(0, end);
+                    return CommandTemplates[command];
+                }
+            }
+
+            return DefaultUnknownTemplate;
+        }
+
+        private static bool IsPrefixKnown(char prefixCandidate)
+        {
+            return DefaultTemplates.ContainsKey(prefixCandidate);
+        }
+    }
+}
