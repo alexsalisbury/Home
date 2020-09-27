36e5801f14af1f2abc15de96e5ddc94286230ac4
Alex Salisbury <alex@alexsalisbury.com>
2020-09-27 11:01:23 -0400

    Add config and logging.

--- src/Home.Service/Program.cs
+++ src/Home.Service/Program.cs
@@ -1,17 +1,43 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-
 namespace Home.Service
 {
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Serilog;
+    using Home.Core.Models.Settings;
+
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
+            var configuration = await LoadConfig();
+            SetupLogging(configuration);
+
+            //Check statement for debugging use.
+            var azureSettings = configuration.GetSection("Azure").Get<AzureSettings>();
+            var botSettings = configuration.GetSection("BotSettings").Get<BotSettings>();
             CreateHostBuilder(args).Build().Run();
+        }
+
+        private static async Task<IConfiguration> LoadConfig()
+        {
+            // TODO: Config by Environment. Read from A: Drive?
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddUserSecrets<Program>();
+
+            return builder.Build();
+        }
+
+        private static void SetupLogging(IConfiguration configuration)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            //TODO: Log relevant settings on startup.
+            Log.Information("Online.");
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
--- src/Home.Service/Worker.cs
+++ src/Home.Service/Worker.cs
@@ -1,28 +1,26 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-
 namespace Home.Service
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.Hosting;
+    using Serilog;
+
     public class Worker : BackgroundService
     {
-        private readonly ILogger<Worker> _logger;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker()
         {
-            _logger = logger;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                Log.Information("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(10000, stoppingToken);
             }
         }
     }
