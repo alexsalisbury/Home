b3b0bc9f05b2754c7820c4e51e522a4dc9609dc8
Alex Salisbury <alex@alexsalisbury.com>
2020-10-10 10:07:47 -0400

    Add Home Command as base Command object.

---
+++ src/Home.Core/Commands/HomeCommand.cs
@@ -0,0 +1,19 @@
+﻿namespace Home.Core.Commands
+{
+    using System.Threading.Tasks;
+
+    public abstract class HomeCommand
+    {
+        /// <summary>
+        /// The command this represents. 
+        /// </summary>
+        public string Command { get; private set; }
+
+        public HomeCommand(string command)
+        {
+            this.Command = command;
+        }
+
+        public abstract Task<bool> ExecuteCommandAsync();
+    }
+}
