c51833d310449c7c4432bbfbfca93a016e2c811a
Alex Salisbury <alex@alexsalisbury.com>
2020-11-07 17:53:31 -0500

    Add Data Actions

---
+++ src/Home.Core/Interfaces/Data/Actions/IFetchable.cs
@@ -0,0 +1,32 @@
+﻿namespace Home.Core.Interfaces.Data.Actions
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Represents a store that can fetch a specific (or all) record(s).
+    /// </summary>
+    /// <typeparam name="T">The Type of record</typeparam>
+    public interface IFetchable<T>
+    {
+        /// <summary>
+        /// Fetches all records of this type
+        /// </summary>
+        /// <returns>a collection of records</returns>
+        Task<IQueryable<T>> Fetch();
+
+        /// <summary>
+        /// Fetches a single record by id
+        /// </summary>
+        /// <param name="id">the id of the record</param>
+        /// <returns>record if found</returns>
+        Task<T> Fetch(int id);
+
+        /// <summary>
+        /// Fetches a single record by id
+        /// </summary>
+        /// <param name="id">the id of the record</param>
+        /// <returns>record if found</returns>
+        Task<T> Fetch(ulong id);
+    }
+}
