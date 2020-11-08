cbf0b1fc860206dd251958903a8d0599e7204627
Alex Salisbury <alex@alexsalisbury.com>
2020-11-08 07:45:28 -0500

    Add ChannelDto and ChannelRepository stubs.

--- src/Home.Core.DiscordBot/Interfaces/Repositories/IChannelRepository.cs
+++ src/Home.Core.DiscordBot/Interfaces/Repositories/IChannelRepository.cs
@@ -1,6 +1,9 @@
 ﻿namespace Home.Core.DiscordBot.Interfaces.Repositories
 {
-    public interface IChannelRepository
+    using Home.Core.DiscordBot.Interfaces.Models;
+    using Home.Core.Interfaces.Data.Actions;
+
+    public interface IChannelRepository: IFetchable<IChannelInfo>, IEnsurable<IChannelInfo>, IPatchable<IChannelInfo>
     {
 
     }
