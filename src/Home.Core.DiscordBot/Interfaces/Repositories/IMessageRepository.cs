f2ace43b603070dbab6b7e7c6af35023585da492
Alex Salisbury <alex@alexsalisbury.com>
2020-11-08 09:23:37 -0500

    Add MessageInfo Dto and Repository.

---
+++ src/Home.Core.DiscordBot/Interfaces/Repositories/IMessageRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Home.Core.DiscordBot.Interfaces.Repositories
+{
+    using Home.Core.DiscordBot.Interfaces.Models;
+    using Home.Core.Interfaces.Data.Actions;
+
+    public interface IMessageRepository : IEnsurable<IMessageInfo>
+    {
+
+    }
+}
