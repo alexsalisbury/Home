9f102e424ebac412ab75cb46384583a89ece327d
Alex Salisbury <alex@alexsalisbury.com>
2020-11-14 10:50:33 -0500

    Build out Channel, Message, User models.

--- src/Home.Core.DiscordBot/Interfaces/Models/IMessageInfo.cs
+++ src/Home.Core.DiscordBot/Interfaces/Models/IMessageInfo.cs
@@ -1,7 +1,15 @@
 ﻿namespace Home.Core.DiscordBot.Interfaces.Models
 {
-    public interface IMessageInfo
-    {
+    using Discord;
 
+    public interface IMessageInfo : ISnowflakeEntity
+    {
+        ulong AuthorId { get; }
+        ulong ChannelId { get; }
+        ulong? GuildId { get; }
+        bool IsPinned { get; }
+
+        // TODO: Reactions - seperate interface?
+        // TODO: IContentHolder interface
     }
 }
--- src/Home.Core.DiscordBot/Models/Data/ChannelInfoDto.cs
+++ src/Home.Core.DiscordBot/Models/Data/ChannelInfoDto.cs
@@ -1,9 +1,20 @@
 ﻿namespace Home.Core.DiscordBot.Models.Dtos
 {
+    using System;
     using Home.Core.DiscordBot.Interfaces.Models;
 
-    public class ChannelInfoDto: IChannelInfo
+    public class ChannelInfoDto : IChannelInfo
     {
+        public ulong? CategoryId { get; }
 
+        public ulong? GuildId { get; }
+
+        public bool IsNsfw { get; }
+
+        public int Position { get; }
+
+        public DateTimeOffset CreatedAt { get; }
+
+        public ulong Id { get; }
     }
 }
