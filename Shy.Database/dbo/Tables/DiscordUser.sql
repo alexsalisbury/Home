8a84ab99e531996cd2a211a0379192dccbfdd3fb
Alex Salisbury <alex@alexsalisbury.com>
2020-12-05 10:38:29 -0500

    Add starter tables.

---
+++ Shy.Database/dbo/Tables/DiscordUser.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE [dbo].[DiscordUser] (
+    [ShyId]              INT                IDENTITY (1, 1) NOT NULL,
+    [Id]                 VARBINARY (8)      NOT NULL,
+    [Username]           NVARCHAR (32)      NOT NULL,
+    [DiscriminatorValue] NVARCHAR (4)       NOT NULL,
+    [IsBot]              BIT                CONSTRAINT [DF_DiscordUser_IsBot] DEFAULT ((0)) NOT NULL,
+    [IsExcluded]         BIT                CONSTRAINT [DF_DiscordUser_IsExcluded] DEFAULT ((1)) NOT NULL,
+    [IsWebhook]          BIT                CONSTRAINT [DF_DiscordUser_IsWebhook] DEFAULT ((0)) NOT NULL,
+    [AvatarId]           NVARCHAR (128)     NULL,
+    [CreatedAt]          DATETIMEOFFSET (7) NOT NULL
+);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [IX_DiscordUser_Id]
+    ON [dbo].[DiscordUser]([Id] ASC);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [IX_DiscordUser_UserName]
+    ON [dbo].[DiscordUser]([Username] ASC);
+
