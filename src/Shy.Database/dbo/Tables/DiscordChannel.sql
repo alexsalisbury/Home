19189f5f8bb234b1beb58da78ca8a2cf34576880
Alex Salisbury <alex@alexsalisbury.com>
2020-12-19 10:56:38 -0500

    Move  Shy.Database to src folder.

--- src/Shy.Database/dbo/Tables/DiscordChannel.sql
+++ src/Shy.Database/dbo/Tables/DiscordChannel.sql
@@ -4,12 +4,15 @@
     [Id]              VARBINARY (8)      NOT NULL,
     [CategoryId]      VARBINARY (8)      NULL,
     [GuildId]         VARBINARY (8)      NULL,
+    [Name]            NVARCHAR (100)     NOT NULL,
     [IsNsfw]          BIT                CONSTRAINT [DF_DiscordChannel_IsNsfw] DEFAULT ((0)) NOT NULL,
     [IsShyRpgChannel] BIT                CONSTRAINT [DF_DiscordChannel_IsShyRpgChannel] DEFAULT ((0)) NOT NULL,
     [IsUserDM]        BIT                CONSTRAINT [DF_DiscordChannel_IsUserDM] DEFAULT ((0)) NOT NULL,
     [Position]        INT                NOT NULL,
     [CreatedAt]       DATETIMEOFFSET (7) NOT NULL
 );
+
+
 
 
 GO
