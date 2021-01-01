0b06e5abc8170ef517afb4521c63bb76574e91d5
Alex Salisbury <alex@alexsalisbury.com>
2021-01-01 17:37:27 -0500

    Add initial channel procs.

---
+++ src/Shy.Database/dbo/Stored Procedures/sb_EnsureDiscordChannel.sql
@@ -0,0 +1,27 @@
+﻿
+CREATE PROCEDURE [dbo].[sb_EnsureDiscordChannel]
+(
+	@ServerShyId int,
+	@Id varbinary(8),
+	@CategoryId varbinary(8),
+	@GuildId varbinary(8),
+	@Name nvarchar(100),
+	@IsNsfw bit,
+	@IsShyRpgChannel bit,
+	@IsUserDM bit,
+	@Position int,
+	@CreatedAt datetimeoffset(7)
+)
+AS
+BEGIN
+    -- SET NOCOUNT ON added to prevent extra result sets from interfering with SELECT statements.
+    SET NOCOUNT ON
+
+	IF (NOT EXISTS(SELECT * FROM [dbo].[DiscordChannel] WHERE [Id] = @Id))
+		BEGIN
+
+		INSERT INTO [dbo].[DiscordChannel]
+					([ServerShyId],[Id],[CategoryId],[GuildId],[Name],[IsNsfw],[IsShyRpgChannel],[IsUserDM],[Position],[CreatedAt])
+		VALUES		(@ServerShyId, @Id, @CategoryId, @GuildId, @Name, @IsNsfw, @IsShyRpgChannel, @IsUserDM, @Position, @CreatedAt)
+	END
+END
---
+++ src/Shy.Database/dbo/Stored Procedures/sb_FetchDiscordChannels.sql
@@ -0,0 +1,9 @@
+﻿
+CREATE PROCEDURE [dbo].[sb_FetchDiscordChannels]
+AS
+BEGIN
+    -- SET NOCOUNT ON added to prevent extra result sets from interfering with SELECT statements.
+    SET NOCOUNT ON
+
+	SELECT * FROM [dbo].[DiscordChannel]
+END
