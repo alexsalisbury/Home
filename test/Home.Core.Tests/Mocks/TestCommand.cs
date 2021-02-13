1e54a7512ad070a6a13be0975018f97b0a948a6f
Alex Salisbury <alex@alexsalisbury.com>
2021-02-13 08:09:28 -0500

    Add initial Test work for HomeCommand

---
+++ test/Home.Core.Tests/Mocks/TestCommand.cs
@@ -0,0 +1,29 @@
+﻿namespace Home.Core.Tests.Mocks
+{
+    using System;
+    using Home.Core.Commands;
+    using System.Threading.Tasks;
+
+    public record TestCommand : HomeCommand
+    {
+        public TestCommand() : base("")
+        {
+
+        }
+
+        public override async Task<StageExecutionResult> ExecuteCommandStageAsync()
+        {
+            await Task.Delay(1);
+            return new StageExecutionResult()
+            {
+                CompletedAt = DateTimeOffset.UtcNow,
+                CreatedAt = DateTimeOffset.UtcNow,
+                NewStage = 7,
+                IsComplete = false,
+                Success = true,
+                RetriesRemaining = 0,
+                StartedAt = DateTimeOffset.UtcNow
+            };
+        }
+    }
+}
---
+++ test/Home.Core.Tests/Models/HomeCommand.Tests.cs
@@ -0,0 +1,15 @@
+﻿namespace Home.Core.Tests
+{
+    using Xunit;
+    using Home.Core.Tests.Mocks;
+
+    public class HomeCommand_Tests
+    {
+        [Fact]
+        public void TestHomeCommand()
+        {
+            var tc = new TestCommand();
+            Assert.NotNull(tc);
+        }
+    }
+}
