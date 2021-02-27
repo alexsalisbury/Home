28ce4839b68eb85980cc378244e4ae0b5bf691ca
Alex Salisbury <alex@alexsalisbury.com>
2021-02-27 12:25:42 -0500

    More complicated MockClient tests.

--- test/Home.Core.Tests/Clients/HomeClient.Tests.cs
+++ test/Home.Core.Tests/Clients/HomeClient.Tests.cs
@@ -1,5 +1,6 @@
 ﻿namespace Home.Core.Tests.Clients
 {
+    using System.Threading.Tasks;
     using Xunit;
     using Home.Core.Tests.Mocks;
 
@@ -10,6 +11,35 @@
         {
             var mc = new MockClient(null);
             Assert.NotNull(mc);
+        }
+
+        [Fact]
+        public async Task GetClientTestAsync()
+        {
+            var first = "Basic";
+            var second = "Auth";
+            var mts = new MockTokenService();
+            mts.Result = $"{first} {second}";
+
+            var mc = new MockClient(null, mts);
+            Assert.NotNull(mc);
+            var h = await mc.WrappedGetClientAsync();
+            Assert.NotNull(h);
+        }
+
+        [Fact]
+        public async Task GetHeaderTestAsync()
+        {
+            var first = "Basic";
+            var second = "Auth";
+            var mts = new MockTokenService();
+            mts.Result = $"{first} {second}";
+
+            var mc = new MockClient(null, mts);
+            Assert.NotNull(mc);
+            var (firstActual, secondActual) = await mc.WrappedGetHeaderAsync();
+            Assert.Equal(first, firstActual);
+            Assert.Equal(second, secondActual);
         }
     }
 }
--- test/Home.Core.Tests/Mocks/MockClient.cs
+++ test/Home.Core.Tests/Mocks/MockClient.cs
@@ -1,13 +1,30 @@
 ﻿namespace Home.Core.Tests.Mocks
 {
+    using System.Net.Http;
+    using System.Threading.Tasks;
     using Home.Core.Clients;
     using Home.Core.Models.Settings;
 
     public class MockClient : HomeClient
     {
-        public MockClient(AzureSettings settings): base(settings, new MockTokenService())
+        public MockClient(AzureSettings settings) : base(settings, new MockTokenService())
         {
 
+        }
+
+        public MockClient(AzureSettings settings, MockTokenService tokenService) : base(settings, tokenService)
+        {
+
+        }
+
+        public async Task<HttpClient> WrappedGetClientAsync()
+        {
+            return await this.GetClientAsync(null);
+        }
+
+        public async Task<(string, string)> WrappedGetHeaderAsync()
+        {
+            return await this.GetHeaderAsync();
         }
     }
 }
