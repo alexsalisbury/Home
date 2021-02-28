106d7037f9aabc23f61a270cad18a03915fc0490
Alex Salisbury <alex@alexsalisbury.com>
2021-02-28 08:56:29 -0500

    Initial ShyCloudClient test

---
+++ test/Home.Core.DiscordBot.Tests/Clients/ShyCloudClient.Tests.cs
@@ -0,0 +1,18 @@
+﻿namespace Home.Core.DiscordBot.Tests.Clients
+{
+    using System.Threading.Tasks;
+    using Xunit;
+    using Home.Core.Models.Settings;
+    using Home.Core.Tests.Mocks;
+    using Home.Core.DiscordBot.Clients;
+
+    public class HomeClient_Tests
+    {
+        [Fact]
+        public void BasicClientTest()
+        {
+            var scc = new ShyCloudClient(null);
+            Assert.NotNull(scc);
+        }
+    }
+}
