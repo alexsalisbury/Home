98905ce44bb6fc7be2692c6eaa834bb9a945a7f6
Alex Salisbury <alex@alexsalisbury.com>
2021-02-28 08:19:52 -0500

    Start building common test library.

---
+++ test/Home.Test.Core/Mocks/MockRepository.cs
@@ -0,0 +1,27 @@
+﻿namespace Home.Test.Core.Mocks
+{
+    using System.Data;
+    using System.Data.Common;
+    using Home.Core.Repositories;
+
+    public class MockRepository : BaseRepository
+    {
+        public MockRepository() : base("")
+        {
+        }
+
+        public MockRepository(string connstr) : base(connstr)
+        {
+        }
+
+        public IDbConnection GetConnection()
+        {
+            return this.Connection;
+        }
+
+        public void SetConnection(DbConnection conn)
+        {
+            this.Connection = conn;
+        }
+    }
+}
