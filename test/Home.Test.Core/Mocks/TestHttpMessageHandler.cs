467e2983839bef5ae784b2207c1d527682dbb039
Alex Salisbury <alex@alexsalisbury.com>
2021-02-28 08:19:52 -0500

    Start building common test library.

---
+++ test/Home.Test.Core/Mocks/TestHttpMessageHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace Home.Core.Tests.Mocks
+{
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class TestHttpMessageHandler : HttpMessageHandler
+    {
+        internal HttpResponseMessage ExpectedReponse { get; set; }
+
+        public void SetExpectedResponse(HttpResponseMessage expectedReponse)
+        {
+            this.ExpectedReponse = expectedReponse;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            await Task.CompletedTask;
+            ExpectedReponse.RequestMessage = request;
+            return ExpectedReponse;
+        }
+    }
+}
