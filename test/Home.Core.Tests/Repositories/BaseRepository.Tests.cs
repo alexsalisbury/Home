a08aa975a04b96bbd80ed38a0224185e897e1468
Alex Salisbury <alex@alexsalisbury.com>
2021-03-20 17:01:39 -0400

    Add basic Fetch test for Base Repo. Rename MockRepository...
    ... to avoid name collision with Moq.

--- test/Home.Core.Tests/Repositories/BaseRepository.Tests.cs
+++ test/Home.Core.Tests/Repositories/BaseRepository.Tests.cs
@@ -1,8 +1,14 @@
 ﻿namespace Home.Core.Tests.Repositories
 {
     using System.Data;
+    using System.Linq;
     using Xunit;
     using Home.Test.Core.Mocks;
+    using System.Threading.Tasks;
+    using Moq;
+    using System.Data.Common;
+    using Moq.Dapper;
+    using Dapper;
 
     public class BaseRepository_Tests
     {
@@ -10,7 +16,7 @@
         [Fact]
         public void BasicRepoTest()
         {
-            var mr = new MockRepository();
+            var mr = new MockHomeRepository();
             Assert.NotNull(mr);
             Assert.NotNull(mr.GetConnection());
             Assert.IsAssignableFrom(typeof(IDbConnection), mr.GetConnection());
@@ -19,7 +25,7 @@
         [Fact]
         public void BasicRepoWithConnStrTest()
         {
-            var mr = new MockRepository(fakeconnstr);
+            var mr = new MockHomeRepository(fakeconnstr);
             Assert.NotNull(mr);
             Assert.NotNull(mr.GetConnection());
             Assert.IsAssignableFrom(typeof(IDbConnection), mr.GetConnection());
@@ -28,7 +34,7 @@
         [Fact]
         public void BasicResetTest()
         {
-            var mr = new MockRepository();
+            var mr = new MockHomeRepository();
             var conn = mr.GetConnection();
             mr.SetConnection(null);
             var conn2 = mr.GetConnection();
@@ -39,13 +45,30 @@
         [Fact]
         public void BasicSetTest()
         {
-            var mr = new MockRepository();
+            var mr = new MockHomeRepository();
             var conn = mr.GetConnection();
             mr.SetConnection(null);
             var conn2 = mr.GetConnection();
             mr.SetConnection(conn);
             Assert.Same(conn, mr.GetConnection());
             Assert.NotSame(conn, conn2);
+        }
+
+        [Fact]
+        public async Task FetchTest()
+        {
+            var procName = "example";
+            var expected = new[] { procName };
+            var mr = new MockHomeRepository();
+
+            var connection = new Mock<DbConnection>();
+            connection.SetupDapperAsync(c => c.QueryAsync<string>(It.IsAny<string>(), null, null, null, null)).ReturnsAsync(expected);
+
+            mr.SetConnection(connection.Object);
+            var result = await mr.FetchAsync(procName);
+            Assert.NotNull(result);
+            Assert.Equal(typeof(string), result.First().GetType());
+            Assert.Equal(procName, result.First());
         }
     }
 }
