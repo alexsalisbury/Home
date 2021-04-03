e62dbe8d388535c691aaeff785be3605851b73f6
Alex Salisbury <alex@alexsalisbury.com>
2021-04-03 12:21:01 -0400

    Map out Discord's LogSeverity to Serilog's LogEventLevel

---
+++ src/Home.Core.DiscordBot/LogSeverityExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace Home.Core.DiscordBot
+{
+    using Discord;
+    using Serilog.Events;
+
+    public static class LogSeverityExtensions
+    {
+        public static LogEventLevel GetSerilogEventLevel(this LogSeverity sev)
+        {
+            return sev switch
+            {
+                LogSeverity.Critical => LogEventLevel.Fatal,
+                LogSeverity.Error => LogEventLevel.Error,
+                LogSeverity.Warning => LogEventLevel.Warning,
+                LogSeverity.Verbose => LogEventLevel.Verbose,
+                LogSeverity.Debug => LogEventLevel.Debug,
+                _ => LogEventLevel.Information
+            };
+        }
+    }
+}
