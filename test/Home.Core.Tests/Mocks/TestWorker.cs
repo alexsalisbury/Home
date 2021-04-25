f30cdf85a65b0184f04642c822803a3d5140fba0
Alex Salisbury <alex@alexsalisbury.com>
2021-04-25 05:01:53 -0400

    Start of HomeWorker

---
+++ test/Home.Core.Tests/Mocks/TestWorker.cs
@@ -0,0 +1,28 @@
+﻿namespace Home.Core.Tests.Mocks
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Home.Core.Workers;
+
+    public class TestWorker : HomeWorker
+    {
+        public bool FailNext { get; set; }
+        public bool RetryAll { get; set; }
+        public bool RetryNext { get; set; }
+        public bool CompleteNext { get; set; }
+
+        public override async Task ExecuteWorkerAsync(CancellationToken stoppingToken)
+        {
+            if (FailNext)
+            {
+                throw new TaskCanceledException();
+            }
+            await Task.Delay(1);
+        }
+
+        public override async Task StartupAsync()
+        {
+        }
+    }
+}
