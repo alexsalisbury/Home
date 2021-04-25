8e57916f0e86420b1c5b56c3f0e72836b2bd3836
Alex Salisbury <alex@alexsalisbury.com>
2021-04-25 05:23:31 -0400

    Break out more testable ChatChannel

--- src/Home.Core.DiscordBot/Models/DiscordChannel.cs
+++ src/Home.Core.DiscordBot/Models/DiscordChannel.cs
@@ -1,76 +1,32 @@
 ﻿namespace Home.Core.DiscordBot.Models
 {
-    using System.Threading.Tasks;
     using Discord.WebSocket;
-    using Home.Core.DiscordBot.Interfaces.Models;
-    using Home.Core.Interfaces.Models;
     using Home.Core.DiscordBot.Models.Dtos;
-    using Home.Core.DiscordBot.Models.Settings;
 
     /// <summary>
     /// Represents a Channel on a Discord Server.
     /// </summary>
-    public class DiscordChannel : IHasDto<IChannelInfo>
+    public class DiscordChannel : ChatChannel
     {
-        /// <summary>
-        /// The name of this channel
-        /// </summary>
-        public string Name { get; set; }
         public ISocketMessageChannel Channel { get; private set; }
-        public string Codeword { get; private set; }
-        private ChannelInfoDto cache;
-        private ChannelSettings settings;
-        public IChannelInfo DtoCache => cacheValid ? cache : ToDto();
 
-        private bool cacheValid = false;
-        //[JsonIgnore]
-        /// <summary>
-        /// The goldfish status, if any, of this channel.
-        /// </summary>
-        //public GoldfishStatus Fish { get; set; }
+        ////[JsonIgnore]
+        ///// <summary>
+        ///// The goldfish status, if any, of this channel.
+        ///// </summary>
+        ////public GoldfishStatus Fish { get; set; }
 
         /// <summary>
         /// Constructs a Channel from server
         /// </summary>
         /// <param name="channelName">The Channel Settings</param>
-        public DiscordChannel(string codeword, ISocketMessageChannel channel)
+        public DiscordChannel(string codeword, ISocketMessageChannel channel) : base(channel?.Id ?? 0, codeword, channel?.Name ?? "Unknown" )
         {
             this.Channel = channel;
-            this.Codeword = codeword;
-            this.Name = channel?.Name ?? "Unknown";
         }
 
-        public async Task AddSettingsAsync(ChannelSettings settings)
+        public DiscordChannel(ChannelInfoDto channel) : base(channel)
         {
-            await Task.Delay(1);
-
-            //TODO: Different channel subtypes may have additional config/state to load from ShyCloud. Do that here.
-
-            this.settings = settings;
-            //if (settings?.Fish != null)
-            //{
-            //    this.Fish = new GoldfishStatus(settings.Fish);
-            //    this.Fish.IsAwake = true;
-            //}
-        }
-
-        public IChannelInfo ToDto()
-        {
-            //TODO: Threadsafe? Idempotency helps mitigate that.
-            if (!cacheValid)
-            {
-                cache =
-                    new ChannelInfoDto()
-                    {
-                        Id = Channel.Id,
-                        Name = Channel.Name,
-                        Codeword = this.Codeword,
-                    };
-
-                cacheValid = true;
-            }
-
-            return cache;
         }
     }
 }
---
+++ test/Home.Core.DiscordBot.Tests/Models/ChatChannel.Tests.cs
@@ -0,0 +1,64 @@
+﻿namespace Home.Core.DiscordBot.Tests.Models
+{
+    using System;
+    using Xunit;
+    using Home.Core.DiscordBot.Models;
+    using Home.Core.DiscordBot.Models.Dtos;
+
+    public class ChatChannel_Tests
+    {
+        [Fact]
+        public void BasicChatChannel()
+        {
+            var cc = new ChatChannel(0, string.Empty, null);
+            Assert.NotNull(cc);
+        }
+
+        [Fact]
+        public void PopulatedChatChannel()
+        {
+            var cc = new ChatChannel(1, "Mock", "General");
+            var cache = cc?.DtoCache;
+            Assert.NotNull(cache);
+            Assert.Equal<ulong>(1, cache.Id);
+            Assert.Equal("Mock", cache.Codeword);
+            Assert.Equal("General", cache.Name);
+        }
+
+        [Fact]
+        public void PopulatedCache()
+        {
+            var ci = new ChannelInfoDto()
+            {
+                CategoryId = 3,
+                Codeword = "Mock",
+                CreatedAt = DateTime.UtcNow,
+                GuildId = 1,
+                Id = 2,
+                IsNsfw = true,
+                IsShyRpgChannel = true,
+                IsUserDM = false,
+                Name = "Fake",
+                Position = 4,
+                ServerShyId = 5,
+                ShyId = 6
+            };
+
+            var cc = new ChatChannel(ci);
+            var cache = (ChannelInfoDto)cc?.DtoCache;
+            Assert.NotNull(cache);
+            Assert.Equal(ci.CategoryId, cache.CategoryId);
+            Assert.Equal(ci.Codeword, cache.Codeword);
+            Assert.Equal(ci.CreatedAt, cache.CreatedAt);
+            Assert.Equal(ci.GuildId, cache.GuildId);
+            Assert.Equal(ci.Name, cache.Name);
+            Assert.Equal(ci.Position, cache.Position);
+            Assert.Equal(ci.ServerShyId, cache.ServerShyId);
+            Assert.Equal(ci.ShyId, cache.ShyId);
+            Assert.Equal(ci.Id, cache.Id);
+            Assert.True(cache.IsNsfw);
+            Assert.True(cache.IsShyRpgChannel);
+            Assert.False(cache.IsUserDM);
+        }
+    }
+}
